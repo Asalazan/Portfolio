733e698797f2f8e3cdd5a310a10d148ad5f15713
v0idzzy <accounts@v0idzzy.com>
2026-03-26 17:06:30 -0400

    added video implementation

---
+++ js/video.js
@@ -0,0 +1,57 @@
+fetch("data/video.json")
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('File not found');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+        AddVideo(data);
+    })
+    .catch(error => {
+        console.error('fetch error: ', error);
+    })
+
+
+
+let AddVideo = (data) => {
+    const container = document.querySelector("#video-container");
+
+    let i = 0
+    data.forEach(item => {
+        const video = document.createElement('div');
+        video.id = `video-thumbnail-${i}`;
+        video.className = "display";
+        video.innerHTML = `
+            <a href="#video-${i}" class="btn">
+                <img class="thumbnail" src="${item.thumbnail}">
+                <h2>${item.title}</h2>
+                <p>${item.shortdescription}</p>
+            </a>
+        `;
+        container.appendChild(video);
+
+        const hiddenVideo = document.createElement('div');
+        hiddenVideo.id = `video-${i}`;
+        hiddenVideo.className = "pop-up";
+        hiddenVideo.setAttribute("role", "dialog");
+        hiddenVideo.setAttribute("aria-labelledby", "portfolio-work-pop-up");
+        hiddenVideo.setAttribute("aria-hidden", "false");
+
+        hiddenVideo.innerHTML = `
+        <div class="pop-up-content">
+            <div class="main-content">
+            <iframe width="1200" height="630" src="${item.video}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                <div class="pop-up-text">
+                    <h3>${item.title}</h3>
+                    <p>${item.longdescription}</p>
+                    <a href=#video-thumbnail-1" class="pop-up-close-btn" aria-label="close">Back</a>
+                </div>
+            </div>
+        </div>
+        `
+        document.body.appendChild(hiddenVideo);
+        i ++
+    });
+}
